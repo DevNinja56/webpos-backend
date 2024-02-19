8cdee388d6aabbad64064c875aea6877efbd9cfa
DevNinja56 <shahzaib@Shahzaibs-MacBook-Pro.local>
2024-02-19 16:29:30 +0500

    Returning items from DB directly

--- src/routers/catalogue.js
+++ src/routers/catalogue.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { getItems } = require('../services/catalogue');
+const Item = require('../modals/Item');
 const router = express.Router();
 
 router.get('/items', async(req, res, next) => {
     try{
         req.meta = {url: 'items'}
-        const data = await getItems(req);
-        res.send(data);
+        const items = await Item.find();
+        res.send({"items": items});
     }catch(e){
         next(e);
     }
